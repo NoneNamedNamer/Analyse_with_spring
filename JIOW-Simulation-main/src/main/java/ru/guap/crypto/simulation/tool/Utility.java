a5553003e003e3b1379fe8812c822cf6f9efc967
DOLOMAIN <70888845+NoneNamedNamer@users.noreply.github.com>
2022-11-03 16:00:46 +0300

    Added project for analyse with Spring

---
+++ JIOW-Simulation-main/src/main/java/ru/guap/crypto/simulation/tool/Utility.java
@@ -0,0 +1,10 @@
+package ru.guap.crypto.simulation.tool;
+
+import java.time.format.DateTimeFormatter;
+
+public class Utility {
+
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd";
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+
+}
